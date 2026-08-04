884ad5442a3af95905ab776484eb0126e4182032
Squezer123 <krzysztof.kordiukiewicz@gmail.com>
2026-08-04 10:09:06 +0200

    init: single-spa root-config + react/vue/svelte mfe

---
+++ apps/root-config/src/KK-root-config.js
@@ -0,0 +1,38 @@
+import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
+
+const routes = constructRoutes(microfrontendLayout);
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return import(/* webpackIgnore: true */ name);
+  },
+});
+const layoutEngine = constructLayoutEngine({ routes, applications });
+
+registerApplication({
+  name: "@KK/react-mfe",
+  app: () => System.import("@KK/react-mfe"),
+  activeWhen: ["/products"],
+});
+
+registerApplication({
+  name: "@KK/vue-mfe",
+  app: () => System.import("@KK/vue-mfe"),
+  activeWhen: ["/cart"],
+});
+
+registerApplication({
+  name: "@KK/svelte-mfe",
+  app: () => System.import("@KK/svelte-mfe"),
+  activeWhen: ["/profile"],
+});
+
+applications.forEach(registerApplication);
+layoutEngine.activate();
+start();
